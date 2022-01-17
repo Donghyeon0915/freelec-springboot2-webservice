2d6be36958c56c1d2a842cd9c1ab679da637efed
donghyeon0915 <dongdong0915_@naver.com>
2022-01-17 21:25:05 +0900

    HelloControllerTest.java import 오류 수정

--- src/test/java/com/donghyeon/springboot/web/HelloControllerTest.java
+++ src/test/java/com/donghyeon/springboot/web/HelloControllerTest.java
@@ -1,23 +1,21 @@
 package com.donghyeon.springboot.web;
 
-import org.apache.catalina.security.SecurityConfig;
+import com.donghyeon.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) //SpringBoot 테스트와 JUnit(자바 테스트 도구) 사이의 연결자 역할
 @WebMvcTest(controllers = HelloController.class, //Web에 집중할 수 있는 어노테이션(controllers에 HelloController 클래스를 연결)
@@ -26,7 +24,6 @@ import static org.hamcrest.Matchers.is;
         }
 )
 //@MockBean(JpaMetamodelMappingContext.class) //JpaAuditing으로 인해 Jpa Bean이 필요
-
 
 public class HelloControllerTest {
     @Autowired //스프링이 관리하는 빈(Bean)을 주입 받음
